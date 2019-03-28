258c8c60f175d7396c90a8c5b6ff2fc83703d8ea
Donghwa96 <ensk960405@gmail.com>
2019-03-28 23:25:59 +0900

    피라미드 만들기

---
+++ hello.py
@@ -0,0 +1,22 @@
+# print("hello world")
+#
+# a= [1, 2, 3, 4, 5]
+#
+# for i in range(1, 10):
+#     print("%" * 10, "{}".format(i), "단")
+#     for j in range(1, 10):
+#         print(i * j)
+#
+#
+# for i in range(1, 10):
+#     for j in range(1,10) :
+#         for k in range(1, 10):
+#             print(i*j*k)
+
+height= input("피라미드의 높이 : ")
+hei = int(height)
+for k in range(1, hei):
+    print("{}".format((hei-k)*" "), "{}".format((2*k-1)*"*"), "{}".format((hei-k)*" "))
+
+
+
